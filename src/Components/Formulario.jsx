563cf1f77c4dd347ebba52d3766b8318b62ab895
Luis Jorge P.S <lpablo@toshiba.com>
2021-07-01 00:54:47 -0500

    Comporatmiendo del fforumario

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -1,33 +1,50 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Formulario = () => {
+    const [nombre, setNombre] = useState('')
+    const [cantidad, setCantidad] = useState(0)
+    // Cuando el usuario agrega un gasto
+    const agregarGasto = e =>{
+        e.preventDefault()
+        alert("Tratando de enviar")
+        // Validar
+        // Construir el gasto
+        // Pasar le gasto al componente princial
+        // Resetar el formulario
+    }
     return (
         <Fragment>
-            <h3>Agrega tus gastos aqui</h3>
-            <div className="campo">
-                <label htmlFor="">Nombre del gasto</label>
+            <form onSubmit={agregarGasto}>
+                <h3>Agrega tus gastos aqui</h3>
+                <div className="campo">
+                    <label htmlFor="">Nombre del gasto</label>
+                    <input 
+                        type="text" 
+                        name="" 
+                        id="" 
+                        className="u-full-width"
+                        placeholder="Ejemplo Transporte"
+                        value={nombre}
+                        onChange={e=>setNombre(e.target.value)}
+                    />
+                </div>
+                <div className="campo">
+                    <label htmlFor="">Cantidad del gasto</label>
+                    <input 
+                        type="number" 
+                        name="" 
+                        id="" 
+                        className="u-full-width"
+                        placeholder="Ej. 300"
+                        value={cantidad}
+                        onChange={e=> setCantidad(parseInt(e.target.value, 10))}
+                    />
+                </div>
                 <input 
-                    type="text" 
-                    name="" 
-                    id="" 
-                    className="u-full-width"
-                    placeholder="Ejemplo Transporte"
-                />
-            </div>
-            <div className="campo">
-                <label htmlFor="">Cantidad del gasto</label>
-                <input 
-                    type="number" 
-                    name="" 
-                    id="" 
-                    className="u-full-width"
-                    placeholder="Ej. 300"
-                />
-            </div>
-            <input 
-                className="btn button-primary u-full-width"
-                type="submit" 
-                value="Agregar Gasto"/>
+                    className="btn button-primary u-full-width"
+                    type="submit" 
+                    value="Agregar Gasto"/>
+            </form>
         </Fragment>
     )
 }
